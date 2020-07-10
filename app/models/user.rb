7e7211416bb88b99f2ac07f06d3031ea6d95c3ea
ismailaidar <ismailaidar@gmail.com>
2020-07-10 14:02:51 -0400

    add test to user email + validate unique|lenght|formar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length: {maximum: 20}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+                        format: {with: VALID_EMAIL_REGEX}, 
+                        length: { maximum: 255 },
+                        uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'Ismail', email: "ismail@memail.com")
+    @user.save
   end
 
   test "sould be valid" do
@@ -17,5 +18,25 @@ class UserTest < ActiveSupport::TestCase
   test "should have an email" do
     @user.email = ""
     assert_not @user.valid?
+  end
+
+  test "name is too long" do
+    @user.name = "s"*50
+    assert_not @user.valid?
+  end
+
+  test "email address is not valid" do
+    @user.email = "xd"
+    assert_not @user.valid?
+  end
+
+  test "email address is too long" do
+      @user.email = "xd"*300
+      assert_not @user.valid?
+  end
+
+  test "email addresse should be unique" do
+    duplicate_user = User.new(name:"test", email: @user.email)
+    assert_not duplicate_user.valid?
   end
 end

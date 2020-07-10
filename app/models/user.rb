50bd8252cfd34833c1b8fc75639df8a8905a8ad7
ismailaidar <ismailaidar@gmail.com>
2020-07-10 14:43:17 -0400

    add validate, test, hash to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
                         format: {with: VALID_EMAIL_REGEX}, 
                         length: { maximum: 255 },
                         uniqueness: true
+    validates :password, presence: true, 
+                        length: { minimum: 6 }
+    has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Ismail', email: "ismail@memail.com")
+    @user = User.new(name: 'Ismail', 
+                      email: "ismail@memail.com", 
+                      password: "foobar", 
+                      password_confirmation: "foobar")
     @user.save
   end
 
@@ -38,5 +41,15 @@ class UserTest < ActiveSupport::TestCase
   test "email addresse should be unique" do
     duplicate_user = User.new(name:"test", email: @user.email)
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
